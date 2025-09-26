86bccd7e20a5674d8319998e398ab2f2b0e7e458
vinoth.nv <vinoth.nv@zohocorp.com>
2025-09-26 12:27:57 +0530

    2.0.1

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ZDHelperKit", url: "https://static.zohocdn.com/zohodeskmobile/ZDHelperKit/iOS/2.0.0/ZDHelperKit.zip", checksum: "3b503cba3fc2e1749eb62ba6ee780fda5cb1fa794336ef630f7cb81708b1d6d8")
+        .binaryTarget(name: "ZDHelperKit", url: "https://static.zohocdn.com/zohodeskmobile/ZDHelperKit/iOS/2.0.1/ZDHelperKit.zip", checksum: "3dfa26e837ed398a76e38ea9fca0b3b733a0afb2527760a1ca173fc3d29aaa81")
     ]
 )
